0404b8d31cc1fa28fe741761ad070797c3986a12
Roseokpe <roseokpe@gmail.com>
2022-09-15 18:19:08 -0700

    Add multiple table

--- data.sql
+++ data.sql
@@ -16,3 +16,10 @@ INSERT INTO animals (name,date_of_birth, weight_kg, neutered, escape_attempts) V
 INSERT INTO animals (name,date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Blossom', '1998-10-13', 17, true, 3);
 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Ditto', '2022-05-14', 22, true, 4);
+
+-- insert data in owners table
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+
+-- species
+INSERT INTO species (name) VALUES ('pokemon'), ('Digimon');
--- queries.sql
+++ queries.sql
@@ -55,3 +55,33 @@ SELECT neutered, COUNT(escape_attempts) AS escape_count FROM animals GROUP BY ne
 SELECT species, MIN(weight_kg) AS MIN_WEIGHT, MAX(weight_kg) AS MAX_WEIGHT FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
+
+-- Project 3
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+-- Sam smith
+UPDATE animals set owners_id = 1 WHERE name = 'Agumon';
+--Jennifer
+UPDATE animals set owners_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+--Bob
+UPDATE animals set owners_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+--Melody 
+UPDATE animals set owners_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+--Dean 
+UPDATE animals set owners_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
+
+--Answers to the questions.
+SELECT animals.name FROM animals JOIN owners ON animals.owners_id = owners.id WHERE owners.full_name = 'Melody Pond';
+
+SELECT animals.name FROM animals JOIN species ON animals.species_id = species.id WHERE species.name = 'pokemon';
+
+SELECT animals.name, owners.full_name FROM animals RIGHT JOIN owners ON animals.owners_id = owners.id;
+
+SELECT species.name, COUNT(animals.species_id) FROM animals JOIN species ON species.id = animals.species_id GROUP BY species.name;
+
+SELECT animals.name from animals JOIN owners ON owners.id = animals.owners_id JOIN species on species.id = animals.species_id WHERE animals.species_id = '2' AND animals.owners_id = '2';
+
+SELECT animals.name from animals JOIN owners ON owners.id = animals.owners_id WHERE animals.escape_attempts = '0' AND animals.owners_id = '5';
+
+SELECT full_name, COUNT(owners_id) FROM owners JOIN animals on owners.id = animals.owners_id GROUP BY full_name ORDER BY COUNT (owners_id) desc limit 1;
