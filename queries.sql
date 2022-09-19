3897309c7e183322e269518f3c445cd6c40ed2f2
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-09-19 16:26:17 +0100

    Analyze the email column from the owners table

--- queries.sql
+++ queries.sql
@@ -119,3 +119,4 @@ SELECT species.name, COUNT(*) FROM species INNER JOIN animals ON animals.species
 -- Performance
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
 EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
